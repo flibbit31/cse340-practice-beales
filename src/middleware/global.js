a5ec767fcbfeb88a59553b673430cb31aeeb036e
flibbit31 <flibbit31@yahoo.com>
2026-02-13 18:35:00 -0700

    Finished Login System Preparation

--- src/middleware/global.js
+++ src/middleware/global.js
@@ -64,6 +64,12 @@ const addLocalVariables = (req, res, next) => {
     // Give access to asset management
     setHeadAssetsFunctionality(res);
 
+    // Convenience variable for UI state based on session state
+    res.locals.isLoggedIn = false;
+    if (req.session && req.session.user) {
+        res.locals.isLoggedIn = true;
+    }
+
     // Continue to the next middleware or route handler
     next();
 };
